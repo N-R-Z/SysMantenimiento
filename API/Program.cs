83d71a9fa9b418d45580937bb2627e0cf188489f
. <167243163+N-R-Z@users.noreply.github.com>
2025-03-25 04:00:09 -0400

    agregado frontend y cors

--- API/Program.cs
+++ API/Program.cs
@@ -9,7 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<MantenimientoDbContext>();
+
 var app = builder.Build();
+
+app.UseCors(builder => builder
+     .AllowAnyOrigin()
+     .AllowAnyMethod()
+     .AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
